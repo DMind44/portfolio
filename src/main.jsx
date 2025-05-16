b0c93ed4aa3d8fd548a5686e0a7f3e35f5c7d2d2
DMind44 <davidmindlin4@gmail.com>
2025-05-16 12:29:17 -0700

    switched to Hashrouter from BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 import About from './components/About'
 import './style.css'
